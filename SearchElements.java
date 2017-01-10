1bd7483756f9db8152f42b7d4d00ccbc51465809
Dannyhh <dobbykiko@gmail.com>
2017-01-10 13:39:24 -0800

    Add files via upload (#4)
    Added Search functionality

--- SearchElements.java
+++ SearchElements.java
@@ -5,7 +5,7 @@
  * in this state, it doesn't work as we have not added
  * the ability to get the string from the textField.
  *
- * @version 6/21/16
+ * @version 12/14/16
  * @author Will Thomas, Danny Higgins
  */
 import java.util.*;
@@ -13,41 +13,69 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-public class SearchElements extends JPanel implements ActionListener {
+public class SearchElements extends JPanel implements ActionListener{
+public static final int numberOfElements = 118;
 private JTextField textField;
-private Scanner elements;
-private String input;
-private Scanner lineBreak;
-private int garbage;
-private double weight;
-private String elementName;
-private String elementSymbol;
+public Scanner elements; //hold the whole text
+public Scanner lineBreak; //holds a line temporarily
+public ArrayList<String> lineList; //holds an entire line
+public ArrayList<String> elementNames;  //holds element names
+public ArrayList<String> elementSymbol; //holds element symbols
+public  String input; //hold the input from the text box
+public double trash; //holds un-needed values
+public Scanner lineBreakTwo; //holds a second copy of a line
+public Scanner elementsTwo; //second copy of elements
 
-   public SearchElements(Scanner elements){
+   public SearchElements(Scanner elements,Scanner elementsTwo){
+      ArrayList<String> elementNames = new ArrayList<String>();
+      ArrayList<String> elementSymbol = new ArrayList<String>();
+      ArrayList<String> lineList = new ArrayList<String>();
+      this.elementNames = elementNames;
+      this.elementSymbol = elementSymbol;
+      this.lineList = lineList;
       this.elements = elements;
+      this.elementsTwo = elementsTwo;
+      listGenerator();
+   }
+   public void listGenerator(){ //creates the 3 array lists for searching
+   for(int i = 0; i < numberOfElements; i++){
+      this.lineBreak = new Scanner(this.elements.nextLine());
+      this.trash = lineBreak.nextInt();
+      this.trash = lineBreak.nextInt();
+      this.trash = lineBreak.nextInt();
+      this.trash = lineBreak.nextDouble();
+      String name = lineBreak.next(); //name of element
+      String symbol = lineBreak.next(); //symbol of element
+      this.elementNames.add(i, name);
+      this.elementSymbol.add(i,symbol);
+      this.lineList.add(this.elementsTwo.nextLine());
+   }
    }
    
    public void createText(CreateButtons panel){ //creates the text field at the top of the table
       textField = new JTextField(20);
       panel.add(textField);
+      textField.addActionListener(this);
    }
    
   @Override
-   public void actionPerformed(ActionEvent ae) {
-      input = textField.getText();
+   public void actionPerformed(ActionEvent e) {
+      this.input = textField.getText();
+      searchScanner();
    }
    
+      
    public void searchScanner(){ //displays the symbol and the element name of the users input
-      this.lineBreak = new Scanner(elements.nextLine());
-      for (int i = 0; i<=2; i++){
-         garbage = lineBreak.nextInt();
+     for(int i = 0; i < numberOfElements; i++){
+      if(this.elementNames.get(i).equalsIgnoreCase(this.input) || this.elementSymbol.get(i).equalsIgnoreCase(this.input)){
+         System.out.println(this.lineList.get(i).toString());
       }
-      weight = lineBreak.nextDouble();
-      elementName = lineBreak.next();
-      elementSymbol = lineBreak.next();
-      if(input.equalsIgnoreCase(elementName) || input.equalsIgnoreCase(elementSymbol)){
-         System.out.print(elementName+" "+elementSymbol);
+
+     }
+      //try creating three array lists, that line up so that position 1 in the names array list 
+      //coresponds to the 1st line of text and first symbol.
+      //ie <Hydrogen, Etc, Etc,> <H, Etc, Etc> <1.0007, Etc, Etc>
+
+
       }
    }
-}
